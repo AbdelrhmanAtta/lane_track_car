7836c6cb15e4b6ba17e7b17160d3632385eadf33
AbdelrhmanAtta <143314426+AbdelrhmanAtta@users.noreply.github.com>
2025-02-19 21:23:22 +0200

    new source with more eff

--- Core/Src/sensor.c
+++ Core/Src/sensor.c
@@ -6,23 +6,12 @@
  */
 #include "sensor.h"
 
-void sensor_init(){
-	/*Configuration for enabling continuous mode in both left and right sensors*/
-	SENSOR_LEFT->Init.ContinuousConvMode = ENABLE;
-	HAL_ADC_Init(SENSOR_LEFT);
-	HAL_ADC_Start(SENSOR_LEFT);
 
-	SENSOR_RIGHT->Init.ContinuousConvMode = ENABLE;
-	HAL_ADC_Init(SENSOR_RIGHT);
-	HAL_ADC_Start(SENSOR_RIGHT);
-
-}
-
-int sensor_read(int pin){
-	long reading;
-	HAL_ADC_PollForConversion(pin, HAL_MAX_DELAY);
-	reading = HAL_ADC_GetValue(pin);
-	return reading;
+int sensor_read(ADC_HandleTypeDef *hadc){
+    HAL_ADC_Start(hadc);
+    if (HAL_ADC_PollForConversion(hadc, 10) == HAL_OK) {
+        return HAL_ADC_GetValue(hadc);
+    }
 }
 
 
