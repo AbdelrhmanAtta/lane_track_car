735724835c2ac21edd6b6fd5750a6d734fecd713
AbdelrhmanAtta <143314426+AbdelrhmanAtta@users.noreply.github.com>
2025-02-19 21:22:04 +0200

    sensor.h updated with enum

--- Core/Inc/sensor.h
+++ Core/Inc/sensor.h
@@ -15,11 +15,21 @@ extern ADC_HandleTypeDef hadc1;
 extern ADC_HandleTypeDef hadc2;
 
 
-/*
+/*//////
  * Defines for sensors channels
  */
-#define SENSOR_LEFT 			&hadc1
-#define SENSOR_RIGHT 			&hadc2
+#define SENSOR_LEFT 			&hadc2
+#define SENSOR_RIGHT 			&hadc1
+
+/*
+ * Enum with return codes for trigger
+ */
+typedef enum{
+	R_NOT_ON_LINE = 0,
+	R_LEFT_DETECTED,
+	R_RIGHT_DETECTED,
+	R_BOTH_DETECTED
+}sensor_trigger_t;
 
 /*
  * @brief: Continous Analog init to save space in code.
@@ -33,14 +43,14 @@ void sensor_init();
  * @param: Sensor pin
  * @return: Analog reading as int
  */
-int sensor_read(int pin);
+int sensor_read(ADC_HandleTypeDef *hadc);
 
 // 7oto sensor_trigger_left w sensor_trigger_right (output 1 lw only one pin et2ra) w sensor_triggered_both (lw 2 pins) 3shan mksl :)
 //separate functions
 //lw 2l 1000-> white  zad alf  1000->black &delay 1 milli
 
-int sensor_trigger_left();
+sensor_trigger_t sensor_trigger_left();
 
-int sensor_trigger_right();
+sensor_trigger_t sensor_trigger_right();
 
 #endif /* INC_SENSOR_H_ */
