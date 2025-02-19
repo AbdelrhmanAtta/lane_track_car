e7dfb09b0da95cabae2c6f8538ac8b61c3be3d9b
mariammmramy <mariam.said06@eng-st.cu.edu.eg>
2025-02-19 20:40:43 +0200

    initialisation function

--- Core/Src/sensor.c
+++ Core/Src/sensor.c
@@ -6,6 +6,18 @@
  */
 #include "sensor.h"
 
+void sensor_init(){
+	/*Configuration for enabling continuous mode in both left and right sensors*/
+	SENSOR_LEFT->Init.ContinuousConvMode = ENABLE;
+	HAL_ADC_Init(SENSOR_LEFT);
+	HAL_ADC_Start(SENSOR_LEFT);
+
+	SENSOR_RIGHT->Init.ContinuousConvMode = ENABLE;
+	HAL_ADC_Init(SENSOR_RIGHT);
+	HAL_ADC_Start(SENSOR_RIGHT);
+
+}
+
 int sensor_read(int pin){
 	long reading;
 	HAL_ADC_PollForConversion(pin, HAL_MAX_DELAY);
