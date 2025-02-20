fee403f390dea7b22413695930ddb799316a631f
nancyashraff <nancy.sabry05@eng-st.cu.edu.eg>
2025-02-20 15:35:23 +0200

    triggered done

--- Core/Src/sensor.c
+++ Core/Src/sensor.c
@@ -16,32 +16,37 @@ int sensor_read(ADC_HandleTypeDef *hadc){
 
 
 sensor_trigger_t sensor_trigger(){
-	static int prev_reading_left=sensor_read(SENSOR_LEFT);
-	static int prev_reading_right=sensor_read(SENSOR_RIGHT);
+	static int prev_reading_left=0;
+	static int prev_reading_right=0;
+
+	prev_reading_left=sensor_read(SENSOR_LEFT);
+	prev_reading_right=sensor_read(SENSOR_RIGHT);
+
+	sensor_trigger_t res;
 
 	if((SENSOR_LEFT>(prev_reading_left+1000))&&(SENSOR_RIGHT>(prev_reading_right+1000))){  /*Left sensor and Right sensor
 																							detected dark material(line)*/
 		prev_reading_left=sensor_read(SENSOR_LEFT);
 		prev_reading_right=sensor_read(SENSOR_RIGHT);
-		return R_BOTH_DETECTED;
+		res= R_BOTH_DETECTED;
 	}
 	else if((SENSOR_LEFT>(prev_reading_left+1000))&&(SENSOR_RIGHT<(prev_reading_right+1000))){/*Left sensor ONLY
 																							detected dark material(line)*/
 		prev_reading_left=sensor_read(SENSOR_LEFT);
 		prev_reading_right=sensor_read(SENSOR_RIGHT);
-		return R_LEFT_DETECTED;
+		res= R_LEFT_DETECTED;
 	}
 	else if((SENSOR_LEFT<(prev_reading_left+1000))&&(SENSOR_RIGHT>(prev_reading_right+1000))){/*Right sensor ONLY
 																								detected dark material(line)*/
 			prev_reading_left=sensor_read(SENSOR_LEFT);
 			prev_reading_right=sensor_read(SENSOR_RIGHT);
-			return R_RIGHT_DETECTED;
+			res= R_RIGHT_DETECTED;
 		}
 	else{
 		prev_reading_left=sensor_read(SENSOR_LEFT);
 		prev_reading_right=sensor_read(SENSOR_RIGHT);
-		return R_NOT_ON_LINE;
+		res= R_NOT_ON_LINE;
 	}
-
+	return res;
 }
 
