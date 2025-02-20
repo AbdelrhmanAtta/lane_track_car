82440ba89530f13415f0a523cca4961162fcc838
AbdelrhmanAtta <143314426+AbdelrhmanAtta@users.noreply.github.com>
2025-02-20 23:21:59 +0200

    Fixed issues with sensor_trigger function

--- Core/Src/sensor.c
+++ Core/Src/sensor.c
@@ -7,7 +7,7 @@
 #include "sensor.h"
 
 
-int sensor_read(ADC_HandleTypeDefhadc) {
+int sensor_read(ADC_HandleTypeDef *hadc) {
     HAL_ADC_Start(hadc);
     HAL_ADC_PollForConversion(hadc, HAL_MAX_DELAY);  // Wait for conversion
     int value = HAL_ADC_GetValue(hadc);
@@ -16,43 +16,27 @@ int sensor_read(ADC_HandleTypeDefhadc) {
 }
 
 
-sensor_trigger_t sensor_trigger() {
-    static int prev_reading_left = 500;   //Initialise readings
-    static int prev_reading_right = 500;
+sensor_trigger_t sensor_trigger(int right_value, int left_value) {
+    static int prev_reading_left = 1000;
+    static int prev_reading_right = 1000;
 
-    int current_reading_left = sensor_read(SENSOR_LEFT);  //Read values from sensors
-    int current_reading_right = sensor_read(SENSOR_RIGHT);
+    int current_reading_left = right_value;
+    int current_reading_right = left_value;
 
-    int threshold=1000;
-
-    if ((current_reading_left > (prev_reading_left + threshold)) && (current_reading_right > (prev_reading_right + threshold))) {
-    	prev_reading_left = current_reading_left;
-    	prev_reading_right = current_reading_right;
+    if ((current_reading_left > (prev_reading_left + 1000)) && (current_reading_right > (prev_reading_right + 1000))) {
     	return R_BOTH_DETECTED;
     }
-    else if (current_reading_left > (prev_reading_left + threshold)) {
-    	prev_reading_left = current_reading_left;
-    	prev_reading_right = current_reading_right;
-        return R_LEFT_DETECTED;
+    else if (current_reading_left > (prev_reading_left + 1000)) {
+    	return R_LEFT_DETECTED;
     }
-    else if (current_reading_right > (prev_reading_right + threshold)) {
-    	prev_reading_left = current_reading_left;
-    	prev_reading_right = current_reading_right;
-        return R_RIGHT_DETECTED;
-    }
-    else if (current_reading_right < (prev_reading_right - threshold)) {
-    	prev_reading_left = current_reading_left;
-    	prev_reading_right = current_reading_right;
-    	return R_NOT_ON_LINE;
-    }
-    else if (current_reading_left < (prev_reading_left - threshold)) {
-    	 prev_reading_left = current_reading_left;
-    	 prev_reading_right = current_reading_right;
-        return R_NOT_ON_LINE;
+    else if (current_reading_right > (prev_reading_right + 1000)) {
+    	return R_RIGHT_DETECTED;
     }
 
 
-    // Update previous readings
     prev_reading_left = current_reading_left;
     prev_reading_right = current_reading_right;
+
+    return R_NOT_ON_LINE;
+
 }
