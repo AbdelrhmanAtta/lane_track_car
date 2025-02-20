95cd834b8441953b7f64c1e21d1fcded2301314a
nancyashraff <nancy.sabry05@eng-st.cu.edu.eg>
2025-02-20 23:53:16 +0200

    final touches

--- Core/Inc/sensor.h
+++ Core/Inc/sensor.h
@@ -9,7 +9,12 @@
 #ifndef INC_SENSOR_H_
 #define INC_SENSOR_H_
 
+
 #include "main.h"
+
+/*
+ * use the defined handler in main
+ */
 
 extern ADC_HandleTypeDef hadc1;
 extern ADC_HandleTypeDef hadc2;
@@ -18,13 +23,15 @@ extern ADC_HandleTypeDef hadc2;
 /*//////
  * Defines for sensors channels
  */
+
 #define SENSOR_LEFT 			&hadc2
 #define SENSOR_RIGHT 			&hadc1
 
 /*
  * Enum with return codes for trigger
  */
-typedef enum{
+typedef enum
+{
 	R_NOT_ON_LINE = 0,
 	R_LEFT_DETECTED,
 	R_RIGHT_DETECTED,
@@ -36,13 +43,8 @@ typedef enum{
  * @param: none
  * @return: void
  */
-void sensor_init();
 
-/*
- * @brief: Reads analog data
- * @param: Sensor pin
- * @return: Analog reading as int
- */
+
 int sensor_read(ADC_HandleTypeDef *hadc);
 
 /*
