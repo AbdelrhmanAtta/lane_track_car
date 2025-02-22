48bb10611cc064c14fb1120141cb774bbbdb2782
AbdelrhmanAtta <143314426+AbdelrhmanAtta@users.noreply.github.com>
2025-02-22 13:10:31 +0200

    Fixed errors and added steer function

--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -2,42 +2,20 @@
  * motor.c
  *
  *  Created on: Feb 19, 2025
- *      Author: Beshoy Maher										lw esht8lt ektb esmk hna
- *      Sub-Author: A.Atta
+ *      Author: A.Atta & Beshoy Maher
  */
-#include 'motor.h'
+#include "motor.h"
+#include "main.h"
 
 /*
  * @brief: Normal PWM init to save space in code.
  * @param: none
  * @return: void
  */
-void motor_init()
+void motor_init(void)
 {
-   
-        __HAL_RCC_TIM2_CLK_ENABLE();  
-
-        TIM_HandleTypeDef pwm_timer2; 
-        TIM_OC_InitTypeDef my_pwm = { 0 }; 
-
-        pwm_timer2.Instance = TIM2; // Change TIM L enta 3ayzo
-
-        pwm_timer2.Init.Prescaler = 39;  // Change bardo Lw ento 3ayz bs an searched W L2et L range Kws  
-        pwm_timer2.Init.CounterMode = TIM_COUNTERMODE_UP;
-        pwm_timer2.Init.Period = 1000; // as above 
-        pwm_timer2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-
-        HAL_TIM_PWM_Init(&pwm_timer2); 
-
-        my_pwm.OCMode = TIM_OCMODE_PWM1;  
-        my_pwm.Pulse = 0 ; 
-        my_pwm.OCPolarity = TIM_OCPOLARITY_HIGH;   
-
-        HAL_TIM_PWM_ConfigChannel(&pwm_timer2, &my_pwm, TIM_CHANNEL_3); // M4 3aref anhy TIM_channel 
-        HAL_TIM_PWM_Start(&pwm_timer2, TIM_CHANNEL_3);  //as above
-
-
-
+	  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+	  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
 }
 
 
@@ -48,25 +26,8 @@ void motor_init()
  */
 void motor_forward_speed(uint16_t speed) 
 {
-    if (speed > 1000)
-    {
-        speed = 1000;
-    }
-
-        TIM_HandleTypeDef pwm_timer2;  
-        TIM_OC_InitTypeDef my_pwm = { 0 };
-
-         
-        my_pwm.OCMode = TIM_OCMODE_PWM1;
-        my_pwm.Pulse = speed; 
-        my_pwm.OCPolarity = TIM_OCPOLARITY_HIGH;
-
-
-        HAL_TIM_PWM_ConfigChannel(&pwm_timer2, &my_pwm, TIM_CHANNEL_3);
-        HAL_TIM_PWM_Start(&pwm_timer2, TIM_CHANNEL_3);
-
-
-
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, speed);
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, speed);
 }
 
 /*
@@ -74,8 +35,12 @@ void motor_forward_speed(uint16_t speed)
  * @param: Left and Right angle to steer with (from 0 to 90).
  * @return: void (change if needed later)
  */
-void motors_steer(uint16_t, uint16_t);
+void motors_steer(uint16_t steer_left_angle, uint16_t steer_right_angle)
+{
+	uint16_t steer_right = (steer_right_angle * 1023) / 90;
+	uint16_t steer_left = (steer_left_angle * 1023) / 90;
 
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, steer_right);
+	__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, steer_left);
+}
 
-
-#endif /* INC_MOTOR_H_ */
